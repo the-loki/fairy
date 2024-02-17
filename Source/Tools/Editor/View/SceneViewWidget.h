97fb727e0f2a57ab2357a7d65c19e881c64d3bd7
Toomimaya <574706224@qq.com>
2024-02-17 14:07:24 +0800

    add some class.

---
+++ Source/Tools/Editor/View/SceneViewWidget.h
@@ -0,0 +1,15 @@
+//
+// Created by loki on 2024/2/17.
+//
+
+#pragma once
+
+#include <Tools/Editor/View/Widget.hpp>
+
+namespace fairy::tools::editor::view {
+
+class SceneViewWidget : public Widget {
+
+};
+
+}
